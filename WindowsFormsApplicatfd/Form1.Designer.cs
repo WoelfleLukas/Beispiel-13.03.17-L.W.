4932624cca3db34f8611df1119331d2aaeafe142
WölfleLukas <WlfleLukas@U11WKS05>
2017-03-13 09:05:06 +0100

    wreagosrgohöivdskfdhöirir

--- WindowsFormsApplicatfd/Form1.Designer.cs
+++ WindowsFormsApplicatfd/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.neu = new System.Windows.Forms.Button();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // neu
@@ -55,11 +56,22 @@
             this.radioButton1.Text = "radioButton1";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(182, 279);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(501, 543);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.neu);
             this.Name = "Form1";
@@ -73,6 +85,7 @@
 
         private System.Windows.Forms.Button neu;
         private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
